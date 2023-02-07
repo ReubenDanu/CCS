23a62366a3328f18ebcdcba73cd9cc5862e4174e
reuben <reubendanu2535@gmail.com>
2023-02-07 13:38:35 +0700

    initial commit

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "community_complient_services";
+
+$mysqli = new mysqli($host, $user, $password, $db);
+
+if($mysqli->connect_errno){
+    throw new RuntimeException('mysqli connection error: ' . $mysqli->connect_error);
+}
+
+$mysqli->set_charset('utf8mb4');
+?>
